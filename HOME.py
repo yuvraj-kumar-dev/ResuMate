a34e742cb3bc41efdd6eb4be764d67192b3572f0
Yuvraj Kumar <yuvrajkumar.dev@gmail.com>
2025-10-14 21:19:41 +0530

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -16,6 +16,7 @@ hide_st_style = """
             <style>
     
             footer {visibility: hidden;}
+            header {visibility: hidden;}
             </style>
     """
 st.markdown(hide_st_style, unsafe_allow_html=True)
